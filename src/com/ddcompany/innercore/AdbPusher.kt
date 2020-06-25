4980f5db06c89359b77aefd6173dd9f87016561a
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2020-06-25 22:27:19 +0300

    runProcessWithProgressSynchronously changed to run

--- src/com/ddcompany/innercore/AdbPusher.kt
+++ src/com/ddcompany/innercore/AdbPusher.kt
@@ -1,8 +1,9 @@
 package com.ddcompany.innercore
 
 import com.ddcompany.innercore.extensions.mkdir
-import com.intellij.openapi.application.ex.ApplicationManagerEx
+import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.MessageType
 import com.intellij.openapi.ui.popup.Balloon
@@ -26,46 +27,46 @@ object AdbPusher {
         val dir = modsDir + service.dir
         service.lastPath = file.path
 
-        val runnable = Runnable {
-            val indicator = ProgressManager.getInstance().progressIndicator
-            var fraction = 0.0
-            val startTime = System.currentTimeMillis()
-            indicator.text = "Getting Files..."
-            indicator.isIndeterminate = false
+        ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Pushing...") {
+            override fun run(indicator: ProgressIndicator) {
+                var fraction = 0.0
+                val startTime = System.currentTimeMillis()
+                indicator.text = "Getting Files..."
+                indicator.isIndeterminate = false
 
-            val files = this.getForPush(file, service)
-            files.forEachIndexed { index, it ->
-                var okPushed = false
-                while (!okPushed) {
-                    try {
-                        val path = this.formatPath(dir, it, project, service.mustPushToRoot)
-                        if (it.isDirectory)
-                            device.mkdir(path)
-                        else device.push(File(it.path), RemoteFile(path))
-                    } catch (e: JadbException) {
-                        continue
+                val files = getForPush(file, service)
+                files.forEachIndexed { index, it ->
+                    var okPushed = false
+                    while (!okPushed) {
+                        try {
+                            val path = formatPath(dir, it, project, service.mustPushToRoot)
+                            if (it.isDirectory)
+                                device.mkdir(path)
+                            else device.push(File(it.path), RemoteFile(path))
+                        } catch (e: JadbException) {
+                            continue
+                        }
+
+                        okPushed = true
                     }
 
-                    okPushed = true
+                    if (indicator.isCanceled)
+                        return
+
+                    indicator.text = "Pushing ($index of ${files.size})..."
+                    indicator.text2 = it.path
+                    indicator.fraction = ++fraction / files.size
                 }
 
-                if (indicator.isCanceled)
-                    return@Runnable
-
-                indicator.text = "Pushing ($index of ${files.size})..."
-                indicator.text2 = it.path
-                indicator.fraction = ++fraction / files.size
+                val delta = System.currentTimeMillis() - startTime
+                val deltaFormatted = if (delta > 1000) (delta / 1000).toString() + "s" else delta.toString() + "ms"
+                JBPopupFactory.getInstance()
+                        .createHtmlTextBalloonBuilder("Successfully pushed in $deltaFormatted ", MessageType.INFO, null)
+                        .createBalloon()
+                        .show(RelativePoint.getCenterOf(WindowManager.getInstance().getStatusBar(project).component), Balloon.Position.atRight)
             }
+        })
 
-            val delta = System.currentTimeMillis() - startTime
-            val deltaFormatted = if (delta > 1000) (delta / 1000).toString() + "s" else delta.toString() + "ms"
-            JBPopupFactory.getInstance()
-                    .createHtmlTextBalloonBuilder("Successfully pushed in $deltaFormatted ", MessageType.INFO, null)
-                    .createBalloon()
-                    .show(RelativePoint.getCenterOf(WindowManager.getInstance().getStatusBar(project).component), Balloon.Position.atRight)
-        }
-
-        ApplicationManagerEx.getApplicationEx().runProcessWithProgressSynchronously(runnable, "", true, project)
     }
 
     fun getDevice(serial: String) =
