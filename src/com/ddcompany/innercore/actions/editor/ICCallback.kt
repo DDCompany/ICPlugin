ea9cc3a4125fb5ad9cbd342e0c8ea2779a1885ee
Dmitriy Medvedev <ddcompanychanel@gmail.com>
2020-07-17 14:38:17 +0300

    Fixed callbacks exception

--- src/com/ddcompany/innercore/actions/editor/ICCallback.kt
+++ src/com/ddcompany/innercore/actions/editor/ICCallback.kt
@@ -1,55 +1,59 @@
 package com.ddcompany.innercore.actions.editor
 
 import com.ddcompany.innercore.ICService
-import com.google.common.io.Resources
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.util.ResourceUtil
 import org.w3c.dom.Element
-import java.io.File
-import java.nio.charset.Charset
 import javax.xml.parsers.DocumentBuilderFactory
 
-val callbackGroups: ArrayList<CallbackGroup> by lazy {
+val callbackGroups: List<CallbackGroup> by lazy {
+    val url = ResourceUtil.getResource(ICCallback::class.java, "callbacks", "chat.xml")
+    if (url == null) {
+        ICService.logger.error("Error while loading callbacks: chat.xml not found")
+        return@lazy emptyList<CallbackGroup>()
+    }
+
+    val dir = VfsUtil.findFileByURL(url)!!.parent
     val list = ArrayList<CallbackGroup>()
-    val dir = File(ICCallback::class.java.getResource("/callbacks").file)
-    println(Resources.getResource("/callbacks"))
-    println(Resources.toString(Resources.getResource("/callbacks"), Charset.forName("UTF-8")))
     val builder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
 
-    dir.listFiles { file ->
-        val document = builder.parse(file)
-        val nodes = document.getElementsByTagName("callback-group")
+    dir.children.forEach { file ->
+        file.inputStream.use { stream ->
+            val document = builder.parse(stream)
+            val nodes = document.getElementsByTagName("callback-group")
 
-        for (i in 0 until nodes.length) {
-            val node = nodes.item(i)
+            for (i in 0 until nodes.length) {
+                val node = nodes.item(i)
 
-            if (node is Element) {
-                if (!node.hasAttribute("name")) {
-                    ICService.logger.error("Invalid name of callback group")
-                    continue
-                }
-
-                val groupName = node.getAttribute("name")
-                val group = list.find { it.name == groupName } ?: CallbackGroup(groupName)
-                val callbackNodes = node.getElementsByTagName("callback")
-
-                for (k in 0 until callbackNodes.length) {
-                    val callbackNode = callbackNodes.item(k)
-                    if (callbackNode is Element) {
-                        if (!callbackNode.hasAttribute("name")) {
-                            ICService.logger.error("Invalid name of callback in '$groupName'")
-                            continue
-                        }
-
-                        val name = callbackNode.getAttribute("name")
-                        val arguments = callbackNode.getAttribute("arguments")
-
-                        group.children.add(ICCallback(name, arguments ?: ""))
+                if (node is Element) {
+                    if (!node.hasAttribute("name")) {
+                        ICService.logger.error("Invalid name of callback group")
+                        continue
                     }
-                }
 
-                list.add(group)
+                    val groupName = node.getAttribute("name")
+                    val group = list.find { it.name == groupName } ?: CallbackGroup(groupName)
+                    val callbackNodes = node.getElementsByTagName("callback")
+
+                    for (k in 0 until callbackNodes.length) {
+                        val callbackNode = callbackNodes.item(k)
+                        if (callbackNode is Element) {
+                            if (!callbackNode.hasAttribute("name")) {
+                                ICService.logger.error("Invalid name of callback in '$groupName'")
+                                continue
+                            }
+
+                            val name = callbackNode.getAttribute("name")
+                            val arguments = callbackNode.getAttribute("arguments")
+
+                            group.children.add(ICCallback(name, arguments ?: ""))
+                        }
+                    }
+
+                    list.add(group)
+                }
             }
         }
-        true
     }
     list
 }
